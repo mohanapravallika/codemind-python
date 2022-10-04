8ea51d9251548523cc2e0ea34000d5326c606814
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-10-04 20:00:03 +0530

    def se(n,x):     for i in range(n):         if x[i]%2==0:             if i%2!=0:                 return False     return True n=int(input()) x=list(map(int,input().split())) print(se(n,x))

---
+++ Strictly_EVEN.py
@@ -0,0 +1,9 @@
+def se(n,x):
+    for i in range(n):
+        if x[i]%2==0:
+            if i%2!=0:
+                return False
+    return True
+n=int(input())
+x=list(map(int,input().split()))
+print(se(n,x))
