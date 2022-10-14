26f49c57931bf8c2f54d43796e3a4bfeb01bfa49
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-10-14 15:55:32 +0530

    n=int(input()) s=0 m=1 for i in range(n):     if n>0:         x=n%10         s=s+x         m=m*x         n=n//10 if m==s:     print("Spy Number") else:     print("Not Spy Number")

---
+++ Spy_Number.py
@@ -0,0 +1,13 @@
+n=int(input())
+s=0
+m=1
+for i in range(n):
+    if n>0:
+        x=n%10
+        s=s+x
+        m=m*x
+        n=n//10
+if m==s:
+    print("Spy Number")
+else:
+    print("Not Spy Number")
