58fdc4d556d5520c808df87f361e7dbc57fa3183
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-02 12:21:04 +0530

    s=input() s=s.split() l=[] c=0 v="aeiouAEIOU" for i in s:     c1=0     for j in i:         if j in v:             c1+=1     l.append(c1) for i in l:     if i==max(l):         c+=1 print(c)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,16 @@
+s=input()
+s=s.split()
+l=[]
+c=0
+v="aeiouAEIOU"
+for i in s:
+    c1=0
+    for j in i:
+        if j in v:
+            c1+=1
+    l.append(c1)
+for i in l:
+    if i==max(l):
+        c+=1
+print(c)
+    
