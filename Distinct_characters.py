a8332fe8eb7da45e645653dab07155bb3d80b561
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-03 18:44:09 +0530

    n=input() n=n.lower() n=n.replace(' ','') c=[] for i in n:     if i not in c:         c.append(i) c=sorted(c) c=''.join(c) print(c)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,14 +1,10 @@
 n=input()
-n=n.replace(" ","")
 n=n.lower()
-l=[]
-c=0
+n=n.replace(' ','')
+c=[]
 for i in n:
-    l.append(i)
-f=[]
-for i in l:
-    if l.count(i)==1:
-        f.append(i)
-f=sorted(f)
-for i in f:
-    print(i,end='')
+    if i not in c:
+        c.append(i)
+c=sorted(c)
+c=''.join(c)
+print(c)
