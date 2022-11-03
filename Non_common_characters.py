a5cc825391677fea678f075669741fd6e96ee6f4
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-03 19:02:40 +0530

    s=input() r=input() s=s.lower() r=r.lower() s=s.replace(' ','') r=r.replace(' ','') c=[] for i in r:     if i not in s:         c.append(i) for i in s:     if i not in r:         c.append(i) f=[] for i in c:     if i not in f:         f.append(i) print(len(f))

---
+++ Non_common_characters.py
@@ -0,0 +1,18 @@
+s=input()
+r=input()
+s=s.lower()
+r=r.lower()
+s=s.replace(' ','')
+r=r.replace(' ','')
+c=[]
+for i in r:
+    if i not in s:
+        c.append(i)
+for i in s:
+    if i not in r:
+        c.append(i)
+f=[]
+for i in c:
+    if i not in f:
+        f.append(i)
+print(len(f))
