72098555daf7b0fe4206571c29a6fac3c90918ca
mohanapravallika <84385928+mohanapravallika@users.noreply.github.com>
2022-11-06 14:24:13 +0530

    def ispri(n):     if n==1:         return False     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     return True n=int(input()) if ispri(n):     if ispri(int(str(n)[::-1])):         print("circular prime")     else:         print("prime but not a circular prime") else:     print("not prime")

---
+++ circular_prime.py
@@ -0,0 +1,15 @@
+def ispri(n):
+    if n==1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+n=int(input())
+if ispri(n):
+    if ispri(int(str(n)[::-1])):
+        print("circular prime")
+    else:
+        print("prime but not a circular prime")
+else:
+    print("not prime")
